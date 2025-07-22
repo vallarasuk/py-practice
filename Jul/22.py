3ab78c3d7d7fab4dbfd231a178fea07bc4e86cd4
vallarasuk <vallarasu.kanthasamy@thidiff.com>
2025-07-22 19:06:27 +0530

    Added create_today_file.py, auto_commit.py

--- Jul/22.py
+++ Jul/22.py
@@ -1,3 +1,31 @@
-print("hello")
-print(20 + 30)
-print("sample word")
+# 2025-07-22 Daily Practice
+# Topic: Printing, Comments, Basic Syntax
+
+"""
+This script practices:
+- Printing outputs
+- Using comments
+- Basic syntax and variable handling
+"""
+
+def main():
+    # Printing
+    print("Hello, World!")
+    print("Today's date:", "2025", "07", "22", sep="-")
+
+    # Variables
+    first_name = "Vallarasu"
+    last_name = "Kanthasamy"
+    age = 24
+    is_coding = True
+
+    # Print all variables
+    print("Name:", first_name, last_name, "| Age:", age, "| Coding:", is_coding)
+
+    # Print data types
+    print("Type of first_name:", type(first_name))
+    print("Type of age:", type(age))
+    print("Type of is_coding:", type(is_coding))
+
+if __name__ == '__main__':
+    main()
